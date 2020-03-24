7fcb6ebcf9884ca45180b1bb7ef80d62818c7d44
angryboo <buyongsong@gmail.com>
2020-03-24 16:42:24 +0900

    1

--- 알고리즘 연습문제/05_to_weird_case.js
+++ 알고리즘 연습문제/05_to_weird_case.js
@@ -1,12 +1,22 @@
 // toWeirdCase함수는 문자열을 인수로 전달받는다.
 // 문자열 s에 각 단어의 짝수번째 인덱스 문자는 대문자로, 홀수번째 인덱스 문자는 소문자로 바꾼 문자열을 리턴하도록 함수를 완성하라.
 
+// 일반 for 문
 function toWeirdCase(s) {
-  const str = [...s];
-  for (let i = 0; i < str.length; i++) {
-    str[i] = i % 2 ? str[i].toLowerCase() : str[i].toUpperCase();
+  const temp = [...s];
+  for (let i = 0; i < temp.length; i++) {
+    temp[i] = i % 2 ? temp[i].toLowerCase() : temp[i].toUpperCase();
   }
-  return str.join('');
+  return temp.join('');
 }
 
+// 배열 고차 함수 응용
 console.log(toWeirdCase('hello world'));
+
+function toWeirdCaseHof(s) {
+  let temp = [...s];
+  temp = temp.map((str, index) => (index % 2 ? str.toLowerCase() : str.toUpperCase()));
+  return temp.join('');
+}
+
+console.log(toWeirdCaseHof('hello world'));
--- 알고리즘 연습문제/06_hide_number.js
+++ 알고리즘 연습문제/06_hide_number.js
@@ -2,6 +2,8 @@
 // 전화번호를 나타내는 문자열 str을 입력받는 hideNumbers 함수를 완성하라
 // 예를들어 s가 ‘01033334444’면 *******4444를 리턴하고, ‘027778888’인 경우는 *****8888을 리턴한다.
 
+
+// 일반 for 문
 function hideNumbers(str) {
   const temp = [...str];
   for (let i = 0; i < temp.length - 4; i++) {
@@ -12,3 +14,14 @@ function hideNumbers(str) {
 
 console.log(hideNumbers('01033334444')); // *******4444
 console.log(hideNumbers('027778888'));
+
+
+// 배열 고차함수 응용
+function hideNumbersHof(str) {
+  let temp = [...str];
+  temp = temp.map((item, index) => ((temp.length - index) <= 4 ? item : '*'));
+  return temp.join('');
+}
+
+console.log(hideNumbersHof('01033334444')); // *******4444
+console.log(hideNumbersHof('027778888'));
