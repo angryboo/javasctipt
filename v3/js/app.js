5d014fbb94eb65ff2f93e9532c9cdb218a9180ff
angryboo <buyongsong@gmail.com>
2020-04-08 15:09:50 +0900

    add todos

--- v3/js/app.js
+++ v3/js/app.js
@@ -1,5 +1,7 @@
 // State
 let todos = [];
+let completedCount = 0;
+let navStatus = 'all';
 
 // DOM
 const $inputTodo = document.querySelector('.input-todo');
@@ -8,40 +10,34 @@ const $activeTodos = document.querySelector('.active-todos');
 const $clearCompleted = document.querySelector('.clear-completed');
 const $completedTodos = document.querySelector('.completed-todos');
 const $completedAll = document.getElementById('ck-complete-all');
-const $all = document.getElementById('all');
-const $active = document.getElementById('active');
-const $completed = document.getElementById('completed');
+const $nav = document.querySelector('.nav');
 
 
 // todo-list count 함수
-const counterTodoList = () => {
-  $activeTodos.textContent = todos.filter(todo => !todo.completed).length;
+const countTodoList = () => {
+  $activeTodos.textContent = todos.length;
 };
 
 // 완료 카운트 함수
 const completedTodos = () => {
-  $completedTodos.textContent = todos.filter(todo => todo.completed).length;
+  $completedTodos.textContent = completedCount;
 };
 
-// 랜더 선택 함수
-const selectRender = todo => {
-  if ($all.className === 'active') return todo;
-  return $active.className === 'active' ? !todo.completed : todo.completed;
-};
-
+// 선택 함수
+const selectRender = todo => (navStatus === 'all' ? todo : navStatus === 'active' ? !todo.completed : todo.completed);
 
 // 렌더함수
 const render = () => {
   let html = '';
-  [...todos].filter(todo => selectRender(todo)).forEach(todo => {
+  todos.filter(todo => selectRender(todo)).forEach(todo => {
     html += `<li id="${todo.id}" class="todo-item">
-    <input id="ck-${todo.id}" class="checkbox" type="checkbox">
+    <input id="ck-${todo.id}" class="checkbox" type="checkbox" ${todo.completed ? 'checked' : ''}>
     <label for="ck-${todo.id}">${todo.content}</label>
     <i class="remove-todo far fa-times-circle"></i>
   </li>`;
   });
   $todos.innerHTML = html;
-  counterTodoList();
+  countTodoList();
   completedTodos();
 };
 
@@ -60,39 +56,45 @@ const removeTodoData = target => {
 };
 
 
-// 완료처리 함수
-const completeTodo = arr => {
-  let tempArr = [];
-  tempArr = todos.filter(todo => arr.some(item => todo.id === item));
-  tempArr = tempArr.map(todo => ({ ...todo, completed: true }));
-  todos = [...todos.filter(todo => arr.every(item => todo.id !== item)), ...tempArr];
-  todos = todos.sort((a, b) => b.id - a.id);
+// checkbox 선택 함수
+
+const selectCheckbox = id => {
+  todos = todos.map(todo => (todo.id === +id ? { ...todo, completed: !todo.completed } : todo));
   render();
 };
 
-// checkbox 선택 함수
-const selectCheckbox = () => {
-  const $checkbox = document.querySelectorAll('.todo-item > .checkbox');
-  const tempArr = [...$checkbox].filter(item => item.checked).map(item => +item.parentElement.id);
-  completeTodo(tempArr);
-};
-
 // all 선택 함수
-const completedAll = (target) => {
-  const $checkbox = document.querySelectorAll('.todo-item > .checkbox');
-
-  [...$checkbox].forEach(item => {
-    item.checked = target.checked || false;
-  });
+const completedAll = checked => {
+  todos = todos.map(todo => ({ ...todo, completed: checked }));
+  render();
 };
 
+
+// todo clear 함수
+const clearCompleted = () => {
+  const temp = todos.length;
+  todos = todos.filter(todo => !todo.completed);
+  completedCount += temp - todos.length;
+  render();
+};
+
+
+// 네비 선택 함수
+const selectNav = target => {
+  [...$nav.children].forEach((item) => {
+    if (target.id === item.id) {
+      item.classList.add('active');
+      navStatus = item.id;
+    } else item.classList.remove('active');
+  });
+  render();
+};
 
 // key up 이벤트
 $inputTodo.onkeyup = (e) => {
   if (e.keyCode !== 13 || !$inputTodo.value) return;
   saveTodoData();
   render();
-  console.log(todos);
   $inputTodo.value = '';
 };
 
@@ -102,40 +104,26 @@ $todos.onclick = e => {
   removeTodoData(e.target);
 };
 
+// checkbox 선택 이벤트
+$todos.onchange = e => {
+  if (!e.target.matches('.todos > .todo-item > .checkbox')) return;
+  selectCheckbox(e.target.parentNode.id);
+};
+
 
 // todo 완료처리 이벤트
 $clearCompleted.onclick = e => {
   if (!e.target.matches('.clear-completed > .btn')) return;
-  selectCheckbox();
-  $completedAll.checked = false;
+  clearCompleted();
 };
 
 // All 선택 Check box 이벤트
 $completedAll.onchange = e => {
-  completedAll(e.target);
-};
-
-// All 선택 이벤트
-$all.onclick = () => {
-  $all.classList.add('active');
-  $active.classList.remove('active');
-  $completed.classList.remove('active');
-  render();
-};
-
-// Active 선택 이벤트
-$active.onclick = () => {
-  $all.classList.remove('active');
-  $active.classList.add('active');
-  $completed.classList.remove('active');
-  render();
+  completedAll(e.target.checked);
 };
 
 
-// Completed 선택 이벤트
-$completed.onclick = () => {
-  $all.classList.remove('active');
-  $active.classList.remove('active');
-  $completed.classList.add('active');
-  render();
+$nav.onclick = ({ target }) => {
+  if (!target.matches('.nav > li')) return;
+  selectNav(target);
 };
